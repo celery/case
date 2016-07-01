32b295ac57ac451250482a8c86d9abc123c6c68b
Ask Solem <ask@celeryproject.org>
2016-07-01 13:53:30 -0700

    Adds bumpversion

--- case/__init__.py
+++ case/__init__.py
@@ -1,20 +1,36 @@
 """Python unittest Utilities"""
 from __future__ import absolute_import, unicode_literals
 
+import re
+
+from collections import namedtuple
+
 from .case import Case
 from .mock import ANY, ContextMock, MagicMock, Mock, call, patch, sentinel
 
 from . import mock
 from . import skip
 
-VERSION = (1, 2, 2)
-__version__ = '.'.join(map(str, VERSION[0:3])) + ''.join(VERSION[3:])
+__version__ = '1.2.2'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://github.com/celery/case'
 __docformat__ = 'restructuredtext'
 
 # -eof meta-
+
+version_info_t = namedtuple(
+    'version_info_t', ('major', 'minor', 'micro', 'releaselevel', 'serial'),
+)
+
+# bumpversion can only search for {current_version}
+# so we have to parse the version here.
+_temp = re.match(
+    r'(\d+)\.(\d+).(\d+)(.+)?', __version__).groups()
+VERSION = version_info = version_info_t(
+    int(_temp[0]), int(_temp[1]), int(_temp[2]), _temp[3] or '', '')
+del(_temp)
+del(re)
 
 __all__ = [
     b'Case',
--- setup.py
+++ setup.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup, find_packages
-    from setuptools.command.test import test
-except ImportError:
-    raise
-    from ez_setup import use_setuptools
-    use_setuptools()
-    from setuptools import setup, find_packages  # noqa
-    from setuptools.command.test import test              # noqa
+from setuptools import setup, find_packages
 
 import os
 import re
@@ -44,30 +36,18 @@ classifiers = [s.strip() for s in classes.split('\n') if s]
 # -*- Distribution Meta -*-
 
 re_meta = re.compile(r'__(\w+?)__\s*=\s*(.*)')
-re_vers = re.compile(r'VERSION\s*=\s*\((.*?)\)')
 re_doc = re.compile(r'^"""(.+?)"""')
-
-
-def rq(s):
-    return s.strip("\"'")
 
 
 def add_default(m):
     attr_name, attr_value = m.groups()
-    return ((attr_name, rq(attr_value)), )
-
-
-def add_version(m):
-    v = list(map(rq, m.groups()[0].split(', ')))
-    return (('VERSION', '.'.join(v[0:3]) + ''.join(v[3:])), )
+    return ((attr_name, attr_value.strip("\"'")),)
 
 
 def add_doc(m):
-    return (('doc', m.groups()[0]), )
+    return (('doc', m.groups()[0]),)
 
-pats = {re_meta: add_default,
-        re_vers: add_version,
-        re_doc: add_doc}
+pats = {re_meta: add_default, re_doc: add_doc}
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'case/__init__.py')) as meta_fh:
     meta = {}
@@ -79,7 +59,7 @@ with open(os.path.join(here, 'case/__init__.py')) as meta_fh:
             if m:
                 meta.update(handler(m))
 
-# -*- Installation Requires -*-
+# -*- Parsing Requirement Files -*-
 
 py_version = sys.version_info
 is_jython = sys.platform.startswith('java')
@@ -108,6 +88,7 @@ def _reqs(*f):
 def reqs(*f):
     return [req for subreq in _reqs(*f) for req in subreq]
 
+# -*- Install Requires -*-
 
 install_requires = reqs('default.txt')
 
@@ -130,14 +111,11 @@ if os.path.exists('README.rst'):
 else:
     long_description = 'See http://pypi.python.org/pypi/case/'
 
-# -*- Entry Points -*- #
-
 # -*- %%% -*-
-
 
 setup(
     name=NAME,
-    version=meta['VERSION'],
+    version=meta['version'],
     description=meta['doc'],
     author=meta['author'],
     author_email=meta['contact'],
